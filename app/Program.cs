48772cd6c292c19e62870880fd32a130222d5964
suvel-the-dev <84700466+suvel-the-dev@users.noreply.github.com>
2022-06-21 17:46:50 +0000

    working

--- app/Program.cs
+++ app/Program.cs
@@ -7,7 +7,20 @@ XmlNodeList catalogs = doc.GetElementsByTagName("catalog");
 
 foreach (XmlNode catalog in catalogs)
 {
- 
-       string newCatalog = catalog.InnerXml;
-    
+
+    XmlNodeList books = catalog.SelectNodes("book");
+
+    foreach (XmlNode book in books)
+    {
+
+        var attribute = book.Attributes["author"];
+        if (attribute != null)
+        {
+            string employeeName = attribute.Value;
+            Console.WriteLine(employeeName);
+            // Process the value here
+        }
+
+    }
+
 }
