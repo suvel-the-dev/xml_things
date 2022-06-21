66104bdec44f0977ca76696e5fabaf6e4a69094a
suvel-the-dev <84700466+suvel-the-dev@users.noreply.github.com>
2022-06-21 17:07:46 +0000

    init

---
+++ app/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml;
+
+XmlDocument doc = new XmlDocument();
+doc.Load("./book.xml");
+
+XmlNodeList catalogs = doc.GetElementsByTagName("catalog");
+
+foreach (XmlNode catalog in catalogs)
+{
+ 
+       string newCatalog = catalog.InnerXml;
+    
+}
